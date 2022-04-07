4534be7d2a4caf51ec157c2880f7528328a14baa
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-04-07 14:54:03 +0600

    create: testimonial section

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,39 @@
-"use strict";
+(function ($) {
+  "use strict";
+
+/*------------------------------------------------------------------
+[Table of contents]
+----------------------------------------
+
+-------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------
+CUSTOM PRE DEFINE FUNCTION
+------------------------------------------------------------*/
+  jQuery.fn.is_exist = function () {
+    return this.length;
+  }
+
+  var testimonial_slider = $('.testimonial-slider');
+  if (testimonial_slider.is_exist()) {
+      testimonial_slider.not(".slick-initialized").slick({
+      dots: false,
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>'
+    });
+  }
+
+
+
+
+})(jQuery);
+
+
+
+
+
