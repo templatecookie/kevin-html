f48e73876213023da1ddd9ea90ce494d4def67ef
hridoypartho <hridoyhalderpartho@gmail.com>
2022-04-13 16:16:44 +0600

    ui page skill section complete

--- app/js/custom.js
+++ app/js/custom.js
@@ -265,7 +265,21 @@
       $(".ui-project-slider--half-fluid").css("margin-left", "0px");
     }
   });
-
+  $(window).scroll(function() {
+    var hT = $('#skill-bar-wrapper').offset().top,
+        hH = $('#skill-bar-wrapper').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS > (hT+hH-1.4*wH)){
+        jQuery(document).ready(function(){
+            jQuery('.skillbar-container').each(function(){
+                jQuery(this).find('.skills').animate({
+                    width:jQuery(this).attr('data-percent')
+                }, 5000); // 5 seconds
+            });
+        });
+    }
+ });
 
 })(jQuery);
 
