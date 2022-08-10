4af7f28abf5b6e778fd5193b06e4c201a74533c9
hridoypartho <hridoyhalderpartho@gmail.com>
2022-08-10 14:34:16 +0600

    resume slider fix"

--- app/js/custom.js
+++ app/js/custom.js
@@ -469,4 +469,18 @@
         ]
       });
   }
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true
+  });
 })(jQuery);
