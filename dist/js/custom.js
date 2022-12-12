6ab34e2718eee283afd95db35a00e6804e6073ae
Rakib0101 <rakib.01jan@gmail.com>
2022-12-12 16:12:40 +0600

    fix: type js

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -492,3 +492,10 @@
         ]
     });
 })(jQuery);
+
+var typed = new Typed('.typed', {
+    strings: ["Full-Stack Developer.", "Javascript Expert."],
+    typeSpeed: 100,
+    loop: true,
+    cursorChar: "|",
+});
